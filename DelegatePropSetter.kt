a5ec256f18468a7f1bff8ec85c44ccc75f738df1
Shivani Gupta <52994096+shivanigupta22@users.noreply.github.com>
2022-03-17 18:36:44 +0530

    Update DelegatePropSetter.kt

--- DelegatePropSetter.kt
+++ DelegatePropSetter.kt
@@ -1,17 +1,56 @@
-class Person(name1: String, var lastname: String) {
-   var updateCount = 0
-   
-   var name: String = name1
-       set(value) {
-           field = value.uppercase()
-           updateCount++
-       }
-}
+//Delegate props are used when want to have a common impl for prop while using setter/getter
+//Delegate props provides helper object for setter/getters
+
+
+import kotlin.reflect.KProperty
 fun main() {
-   val kotlin = Person("xxx","yyyy")
+   val kotlin = Person()
    println(kotlin.name)
    println(kotlin.updateCount)
-  kotlin.name = "www"
+       println(kotlin.lastname)
+
+  kotlin.name = "shivi"
   println(kotlin.name)
  println(kotlin.updateCount)
+    kotlin.lastname = "pandey"
+
+    println(kotlin.lastname)
+
+ println(kotlin.updateCount)
+
+
+}
+
+class Person() {
+   var updateCount = 0 
+//    var name: String = ""
+//        set(value) {
+//            field = value.uppercase()
+//            updateCount++
+//        }
+//       var lastname: String = ""
+//         set(value) {
+//             field = value.uppercase()
+//             updateCount++
+//         }
+//         
+var name:String by Delegate()
+var lastname:String by Delegate()
+
+}
+
+
+class Delegate() {
+       private var formattedString: String = ""
+// using thisRef we can modify the passed instance value
+    operator fun getValue(thisRef: Any?, prop: KProperty<*>): String {        
+        return formattedString
+    }
+
+    operator fun setValue(thisRef: Any?, prop: KProperty<*>, value: String) { 
+         if (thisRef is Person) {
+      			 thisRef.updateCount++
+   }
+        formattedString = value.uppercase()
+    }
 }
