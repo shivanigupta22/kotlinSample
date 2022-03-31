fa9df24bb7ddc2101335c1643e554e87fa0bd5e7
Shivani Gupta <52994096+shivanigupta22@users.noreply.github.com>
2022-03-31 17:41:46 +0530

    Update NestedAndInnerClass.kt

--- NestedAndInnerClass.kt
+++ NestedAndInnerClass.kt
@@ -5,14 +5,18 @@ val demo2 = Outer.Nested2().foo() // == 2
 class Outer {
     private val bar: Int = 1
     init {
-        println(Nested().foo())
-        println(Nested2().foo())
+        println(Nested().foo()) // 1
+        println(Nested2().foo()) // 2
+        println(b1) // not accessible
+        println(b2) // not accessible
     }
     inner class Nested {
+        val b1= 4
         fun foo() = bar // inner class can access outer class members
       fun foo2() = 2
     }
     class Nested2 {
+         val b2= 5
         fun foo() = bar // will not accessible
     }
 }
