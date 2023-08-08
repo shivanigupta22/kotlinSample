47fa16f45765cd5c4852c0d3428785644d7b38c0
Shivani Pandey <52994096+shivanigupta22@users.noreply.github.com>
2023-08-08 11:16:21 +0530

    Create CompanionObjectVsObject.kt

---
+++ CompanionObjectVsObject.kt
@@ -0,0 +1,13 @@
+##Companion Object
+/* 1. Should be defined inside the class only
+2. Can be treated static keyword in java
+3. Can be defined without name and can get called --> ClassName.CompanionMethodName()
+4. Instantiated at the first time when the class is instantiated and this object is common for all instances of the class
+*/
+##Object
+/*
+1. Can be defined anywhere
+2. Can be used for singleton behavior
+3. Cannot be defined without name
+4. Lazily instantiated when used for the first time.
+*/
